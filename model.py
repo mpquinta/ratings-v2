3df53d6b0d939002ab8bde727f8ec50cfdbf58d5
Megan Q <meganquintana30@gmail.com>
2022-07-14 21:01:56 -0700

    7/14/22

--- model.py
+++ model.py
@@ -44,8 +44,13 @@ class Rating(db.Model):
     rating_id = db.Column(db.Integer,
                         autoincrement=True,
                         primary_key=True)
+    score = db.Column(db.Integer)
     movie_id = db.Column(db.Integer, db.ForeignKey("movies.movie_id"))
     user_id = db.Column(db.Integer, db.ForeignKey("users.user_id"))
+
+    movie = db.relationship("Movie", backref="ratings")
+    user = db.relationship("User", backref="ratings")
+
     
 
     def __repr__(self):
